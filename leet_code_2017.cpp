69d8fa14489ea272d4fa9ba8cdea1061515516c3
Movazed <arya6v9000@gmail.com>
2025-01-22 19:56:23 +0530

    54 days

--- leet_code_2017.cpp
+++ leet_code_2017.cpp
@@ -1,20 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-class Solution {
+class Solution
+{
 public:
-    long long gridGame(vector<vector<int>>& grid) {
+    long long gridGame(vector<vector<int>> &grid)
+    {
         ll top = 0;
-        for(int i = 0; i < grid[0].size(); i++){
+        for (int i = 0; i < grid[0].size(); i++)
+        {
             top += grid[0][i];
         }
 
-        ll bottom = 0; ll res = LLONG_MAX;
+        ll bottom = 0;
+        ll res = LLONG_MAX;
 
-        for(int i = 0; i < grid[0].size(); i++){
+        for (int i = 0; i < grid[0].size(); i++)
+        {
             top -= grid[0][i];
             res = min(res, max(top, bottom));
-            bottom += grid[1][i];           
+            bottom += grid[1][i];
         }
         return res;
     }
