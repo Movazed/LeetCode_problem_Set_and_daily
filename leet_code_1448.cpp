984952d67a8ad2d051b757c48dc1bdd9d48f0764
Movazed <arya6v9000@gmail.com>
2025-01-26 19:29:17 +0530

    DFS tree news solutions

--- leet_code_1448.cpp
+++ leet_code_1448.cpp
@@ -1,33 +1,36 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
 
+
+//  Definition for a binary tree node.
+  struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+ 
 class Solution {
 public:
-    int goodNodes(TreeNode* root) {
-        if(root == NULL){
-            return 0;
+    ll count = 0;
+    void dfs(TreeNode* node, ll curMax){
+        if(!node){
+            return;
         }
-        return dfs(root, root->val);
+
+        if(node->val >= curMax){
+            count++;
+            curMax = node->val;
+        }
+
+        dfs(node->left, curMax);
+        dfs(node->right, curMax);
     }
-private:
-    int dfs(TreeNode* root, int maxValue){
-        if(root == NULL){
-            return 0;
-        }
-        int count = 0;
-        if(root->val >= maxValue){
-            count  = 1;
-        }
-        maxValue = max(root->val, maxValue);
-        return count + dfs(root->left, maxValue) + dfs(root->right, maxValue);
+    int goodNodes(TreeNode* root) {
+        dfs(root, root->val);
+        return count;
     }
 };
