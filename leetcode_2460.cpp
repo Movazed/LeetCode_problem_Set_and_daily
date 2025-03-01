86ec3d089ec3cb6d4fae3f5f248f3ad5e412f530
Movazed <arya6v9000@gmail.com>
2025-03-01 10:44:23 +0530

    MARCH 1

---
+++ leetcode_2460.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+class Solution {
+    public:
+        vector<int> applyOperations(vector<int>& nums) {
+            ll n = nums.size();
+             ll inz = 0;
+
+             for(ll i = 0; i < n - 1; i++){
+                if(nums[i] == nums[i + 1]){
+                    nums[i] *= 2;
+                    nums[i + 1]  = 0;
+                }
+             }
+
+             for(ll i = 0; i < n; i++){
+                if(nums[i] != 0){
+                    nums[inz++] = nums[i];
+                }
+             }
+
+             while (inz < n)
+             {
+                    nums[inz++] = 0;
+             }
+             
+             return nums;
+        }
+    };
