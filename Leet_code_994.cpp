34c2ae457524cec333d6b9771d7f149e6089167d
Movazed <arya6v9000@gmail.com>
2025-06-16 15:39:19 +0530

    NEW

--- Leet_code_994.cpp
+++ Leet_code_994.cpp
@@ -1,54 +1,17 @@
-//4 DIRECTIONSS!!!!! BFS  - 0ms
+#include<bits/stdc++.h>
+using namespace std;
+
 
 class Solution {
 public:
-    void move(vector<vector<bool>>& visit, vector<vector<int>>& grid, int x, int y, queue<pair<int, int>>& q, int& fresh){
-        if(x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size() || visit[x][y] || grid[x][y] == 0){
-            return;
-        }
-
-        if(grid[x][y] == 1){
-            visit[x][y] = true;
-            q.push({x, y});
-            fresh--;
-        }
-    }
     int orangesRotting(vector<vector<int>>& grid) {
-        vector<vector<bool>> visit(grid.size(), vector<bool>(grid[0].size(), false));
+        vector<vector<int>> ans = grid;
+        int n = grid.size();
+        int m = grid[0].size();
 
-        queue<pair<int, int>> q;
+        int time = -1;
 
-        int fresh = 0;
-        int minute = 0;
-
-        for(int i  = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[0].size(); j++){
-                if(grid[i][j] == 2){
-                    q.push({i, j});
-                    visit[i][j] = true;
-                } else if (grid[i][j] == 1){
-                    fresh++;
-                }
-            }
-        }
-
-        while(!q.empty()){
-            int size = q.size();
-
-            for(int i = 0; i < size; i++){
-
-                auto [x, y] = q.front();
-                q.pop();
-
-                move(visit, grid, x + 1, y, q, fresh);
-                move(visit, grid, x - 1, y, q, fresh);
-                move(visit, grid, x, y + 1, q, fresh);
-                move(visit, grid, x, y - 1, q, fresh);
-            }
-            if(!q.empty()){
-            minute++;
-            }
-        }
-        return fresh == 0 ? minute : - 1;
+        int delRow = {-1, 0, +1, 0};
+        int delCol = {0, +1, 0, -1};
     }
 };
