3e17ba40248af25b79b4659c2f22babfdd417b2e
Arya Putatunda <136840478+Movazed@users.noreply.github.com>
2025-12-05 20:44:18 +0530

    Time: 53 ms (7.16%), Space: 10.4 MB (13.59%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        for(char &c : s){
+            c = tolower(static_cast<unsigned char>(c));
+        }
+        for (int i = 0; i < s.length(); ) {
+            if (!isalnum(static_cast<unsigned char>(s[i]))) {
+                s.erase(i, 1);  
+            } else {
+                i++;
+            }
+        }
+        string re = s;
+
+         reverse(re.begin(), re.end());
+
+        if(s == re){
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
