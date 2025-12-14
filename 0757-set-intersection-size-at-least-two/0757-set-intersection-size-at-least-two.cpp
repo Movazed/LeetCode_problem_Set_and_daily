b71e6fd5495d201f1d60c1e04b23b704ddd62ed4
Arya Putatunda <136840478+Movazed@users.noreply.github.com>
2025-12-14 10:04:12 +0530

    Time: 7 ms (31.85%), Space: 24.3 MB (6.09%) - LeetHub

---
+++ 0757-set-intersection-size-at-least-two/0757-set-intersection-size-at-least-two.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int intersectionSizeTwo(vector<vector<int>>& intervals) {
+        vector<vector<int>> iv = intervals;
+        sort(iv.begin(), iv.end(), [](auto &a, auto &b){
+            if(a[1] == b[1]) return a[0] > b[0];
+            return a[1] < b[1];
+        });
+        int a = -1, b = -1, ans = 0;
+        for(auto &v : iv){
+            int l = v[0], r = v[1];
+            bool ca = (a >= l), cb = (b >= l);
+            if(ca && cb) continue;
+            if(cb){
+                a = b;
+                b = r;
+                ans += 1;
+            } else {
+                a = r - 1;
+                b = r;
+                ans += 2;
+            }
+        }
+        return ans;
+    }
+};
