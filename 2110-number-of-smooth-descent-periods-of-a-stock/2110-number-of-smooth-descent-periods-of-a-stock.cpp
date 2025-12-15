39c57f80a93089c6faf4eaa400b9890ad90c6e55
Arya Putatunda <136840478+Movazed@users.noreply.github.com>
2025-12-15 13:50:28 +0530

    Time: 0 ms (100%), Space: 107.6 MB (14.13%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -1,25 +1,15 @@
-// C++17 CP style, line commented
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    long long getDescentPeriods(vector<int>& a) {
-        ios::sync_with_stdio(false); 
+    long long getDescentPeriods(vector<int>& prices) {
+        ios::sync_with_stdio(false);
         cin.tie(NULL);
 
-        // ans holds total periods, cnt holds current streak length
-        long long ans = 0, cnt = 0;
-        for (int i = 0; i < (int)a.size(); ++i) {
-            if (i > 0 && a[i - 1] - a[i] == 1) {
-                // continues smooth descent
-                ++cnt;
-            } else {
-                // new streak starts at this day
-                cnt = 1;
-            }
-            ans += cnt; // add all periods ending at i
+        long long res = 1, streak = 1;
+        for (int i = 1; i < prices.size(); ++i) {
+            if (prices[i - 1] - prices[i] == 1) streak++;
+            else streak = 1;
+            res += streak;
         }
-        return ans;
+        return res;
     }
 };
