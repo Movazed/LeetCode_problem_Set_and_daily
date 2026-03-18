3e733d39e5fff45bf709f20e64f52561d42b38f9
Arya Putatunda <136840478+Movazed@users.noreply.github.com>
2026-03-18 23:09:53 +0530

    Time: 0 ms (100%), Space: 89.8 MB (99.44%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -1,16 +1,18 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 using ii=int;
+using ll=long long;
 
 #define fr(i,a,b) for(ii i=(a);i<(b);++i)
 
-template<class t> struct ab{ virtual t f(t)=0; virtual ~ab(){} };
-template<class t> struct im:ab<t>{ t f(t v){ return v; } };
+template<class t> struct ab{ virtual t f(void*,void*)=0; virtual ~ab(){} };
 
-class solution{
-public:
-    static ii f(vector<vector<ii>>& g, ii k){
+template<class t> struct im:ab<t>{
+    t f(void* x,void* y){
+        auto& g=*reinterpret_cast<vector<vector<ii>>*>(x);
+        ii k=*reinterpret_cast<ii*>(y);
+
         ii r=g.size(),c=g[0].size();
         ii ct=0,bc=c;
 
@@ -39,27 +41,53 @@ public:
                 ct++;
             }
         }
+
         return ct;
     }
 };
 
-auto init=[](){
+auto z=[](){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    return 'c';
+    return 'z';
 }();
+
+class solution{
+public:
+    static ii run(vector<vector<ii>>& g,ii k){
+        ab<ii>* o=new im<ii>();
+
+        auto l=[](void* a,void* b,void* c)->ii{
+            return reinterpret_cast<ab<ii>*>(c)->f(a,b);
+        };
+
+        void* p=reinterpret_cast<void*>(&g);
+        void* q=reinterpret_cast<void*>(&k);
+        void* r=reinterpret_cast<void*>(o);
+
+        return ((ii(*)(void*,void*,void*))l)(p,q,r);
+    }
+};
 
 class Solution{
 public:
-    int countSubmatrices(vector<vector<int>>& g, int k){
-        struct a{ virtual ii h(vector<vector<ii>>&,ii)=0; };
+    int countSubmatrices(vector<vector<int>>& g,int k){
+        struct a{ virtual ii h(void*,void*)=0; };
         struct b:a{
-            ii h(vector<vector<ii>>& x, ii y){
-                return ((ii(*)(vector<vector<ii>>&,ii))(&solution::f))(x,y);
+            ii h(void* x,void* y){
+                return solution::run(
+                    *reinterpret_cast<vector<vector<ii>>*>(x),
+                    *reinterpret_cast<ii*>(y)
+                );
             }
         };
-        a* o=new b();
-        return o->h(reinterpret_cast<vector<vector<ii>>&>(g),k);
+
+        a* v=new b();
+
+        void* p=reinterpret_cast<void*>(&g);
+        void* q=reinterpret_cast<void*>(&k);
+
+        return v->h(p,q);
     }
 };
