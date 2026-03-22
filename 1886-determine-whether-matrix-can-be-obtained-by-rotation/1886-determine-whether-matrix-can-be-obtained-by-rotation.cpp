4e972fb8d56022c9aa125e141d4e12c8bcb9cbac
Arya Putatunda <136840478+Movazed@users.noreply.github.com>
2026-03-22 19:51:38 +0530

    Time: 0 ms (100%), Space: 14.8 MB (20.77%) - LeetHub

---
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fr(x, a, b) for (ll x = a; x <= b; x++)
+#define fe(x, c)    for (auto x : c)
+
+typedef long long ll;
+typedef int       ii;
+
+template<typename mt>
+struct rot_t {
+    mt& mg;
+    ii  mn;
+
+    rot_t(mt& g) : mg(g), mn(ii(g.size())) {}
+
+    void go() {
+        fr(i, 0, mn - 1)
+            fr(j, i + 1, mn - 1)
+                swap(mg[ii(i)][ii(j)], mg[ii(j)][ii(i)]);
+        fr(i, 0, mn - 1)
+            reverse(mg[ii(i)].begin(), mg[ii(i)].end());
+    }
+};
+
+struct sol {
+    function<void()>         rf;
+    vector<vector<ii>>*      mp;
+    rot_t<vector<vector<ii>>> rt;
+
+    sol(vector<vector<ii>>& mat, vector<vector<ii>>& tg)
+        : mp(&mat), rt(mat) {
+        rf = [&]() { rt.go(); };
+    }
+
+    bool go(vector<vector<ii>>& tg) {
+        return [&]() -> bool {
+            fr(i, 0, 3) {
+                if (*mp == tg) return true;
+                rf();
+            }
+            return false;
+        }();
+    }
+};
+
+class Solution {
+public:
+    bool findRotation(vector<vector<ii>>& mat, vector<vector<ii>>& target) {
+        return sol(mat, target).go(target);
+    }
+};
