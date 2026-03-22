328f99b71a096cf34686aa04085c045b15754962
Arya Putatunda <136840478+Movazed@users.noreply.github.com>
2026-03-22 19:52:16 +0530

    Time: 0 ms (100%), Space: 14.7 MB (41.55%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -1,52 +1,26 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-#define fr(x, a, b) for (ll x = a; x <= b; x++)
-#define fe(x, c)    for (auto x : c)
-
+#define fr(x,a,b) for(ll x=a;x<=b;x++)
+#define fe(x,c) for(auto x:c)
 typedef long long ll;
-typedef int       ii;
+typedef int ii;
 
-template<typename mt>
-struct rot_t {
-    mt& mg;
-    ii  mn;
+template<typename mt>struct rot_t{
+mt&mg;ii mn;
+rot_t(mt&g):mg(g),mn(ii(g.size())){}
+void go(){
+fr(i,0,mn-1)fr(j,i+1,mn-1)swap(mg[ii(i)][ii(j)],mg[ii(j)][ii(i)]);
+fr(i,0,mn-1)reverse(mg[ii(i)].begin(),mg[ii(i)].end());
+}};
 
-    rot_t(mt& g) : mg(g), mn(ii(g.size())) {}
-
-    void go() {
-        fr(i, 0, mn - 1)
-            fr(j, i + 1, mn - 1)
-                swap(mg[ii(i)][ii(j)], mg[ii(j)][ii(i)]);
-        fr(i, 0, mn - 1)
-            reverse(mg[ii(i)].begin(), mg[ii(i)].end());
-    }
+struct sl{
+function<void()>rf;
+vector<vector<ii>>*mp;
+rot_t<vector<vector<ii>>>rt;
+sl(vector<vector<ii>>&mat,vector<vector<ii>>&tg):mp(&mat),rt(mat){rf=[&](){rt.go();};}
+bool go(vector<vector<ii>>&tg){return[&]()->bool{fr(i,0,3){if(*mp==tg)return true;rf();}return false;}();}
 };
 
-struct sol {
-    function<void()>         rf;
-    vector<vector<ii>>*      mp;
-    rot_t<vector<vector<ii>>> rt;
-
-    sol(vector<vector<ii>>& mat, vector<vector<ii>>& tg)
-        : mp(&mat), rt(mat) {
-        rf = [&]() { rt.go(); };
-    }
-
-    bool go(vector<vector<ii>>& tg) {
-        return [&]() -> bool {
-            fr(i, 0, 3) {
-                if (*mp == tg) return true;
-                rf();
-            }
-            return false;
-        }();
-    }
-};
-
-class Solution {
-public:
-    bool findRotation(vector<vector<ii>>& mat, vector<vector<ii>>& target) {
-        return sol(mat, target).go(target);
-    }
+class Solution{public:
+bool findRotation(vector<vector<ii>>&mat,vector<vector<ii>>&target){return sl(mat,target).go(target);}
 };
