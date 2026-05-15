4bc629c0e50b827289a636ed1afde2318520692d
Arya Putatunda <136840478+Movazed@users.noreply.github.com>
2026-05-15 22:38:41 +0530

    Time: 0 ms (100%), Space: 14.3 MB (10.85%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,18 +1,67 @@
-1class Solution {
-2public:
-3    int findMin(vector<int>& nums) {
-4        int left = 0;
-5        int right = nums.size() - 1;
-6
-7        while(left < right){
-8            int mid = left + (right - left) / 2;
-9
-10            if(nums[mid] <= nums[right]){
-11                right = mid;
-12            } else {
-13                left = mid + 1;
-14            }
-15        }
-16        return nums[left];
-17    }
-18};
+#include<bits/stdc++.h>
+#define ii int
+#define ll long long
+#define vc vector
+#define pb push_back
+#define fr(i,a,b) for(ii i=(a);i<(b);++i)
+#define wl(x) while(x)
+#define re return
+
+using namespace std;
+
+template<class t>
+using cp=t*;
+
+struct ax{
+    virtual ii fx(vc<ii>&)=0;
+};
+
+struct bx:ax{
+
+    ii fx(vc<ii>& nm) override{
+
+        cp<vc<ii>> p=&nm;
+
+        auto fn=[&](ii x)->bool{
+
+            cp<ii> bk=&((*(p)).back());
+
+            re x>(*bk);
+        };
+
+        auto it=partition_point(
+            (*(p)).begin(),
+            (*(p)).end(),
+            [&](ii x)->bool{
+
+                cp<ii> px=&x;
+
+                re fn((*reinterpret_cast<ii*>(px)));
+            }
+        );
+
+        cp<ii> rt=&(*it);
+
+        re (*rt);
+    }
+};
+
+class Solution {
+public:
+
+    int findMin(vector<int>& nums) {
+
+        vc<ii> nm;
+
+        for(auto &x:nums)
+            nm.pb((*reinterpret_cast<ii*>(&x)));
+
+        ax* ob=new bx();
+
+        ii an=((ob!=nullptr)?ob->fx(nm):0);
+
+        delete ob;
+
+        re static_cast<int>(an);
+    }
+};
